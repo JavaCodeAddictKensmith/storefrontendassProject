9a2b101892218c1d5e8e75a83074aef3eb6a70e1
Kenneth Ajah <ajahk@activedgetechnologies.com>
2025-01-31 16:13:46 +0100

    completes creating reusable card

--- src/routes/dashboard/page.jsx
+++ src/routes/dashboard/page.jsx
@@ -2,8 +2,6 @@ import { Search } from "lucide-react";
 
 import { useTheme } from "@/hooks/use-theme";
 
-import StoreSessionsCard from "./StoreSessionCard";
-
 import { Footer } from "@/layouts/footer";
 
 import documentTextIcon from "@/assets/svgs/document-text.svg";
@@ -20,78 +18,6 @@ import Card from "./components/Card";
 
 const DashboardPage = () => {
     // const { theme } = useTheme();
-
-    const dummyData = {
-        headers: [
-            { id: 1, text: "Source" },
-            { id: 2, text: "impressions" },
-            { id: 3, text: "Clicks" },
-            { id: 4, text: "Revenue" },
-        ],
-        rows: [
-            {
-                bidAmount: 1,
-                name: "Facebook",
-                imp: "35",
-                clicks: "5",
-                revenue: "₦ 22,035.00",
-            },
-
-            {
-                bidAmount: 2,
-                name: "Instagram",
-                imp: "35",
-                clicks: "5",
-                revenue: "₦ 22,035.00",
-            },
-            {
-                bidAmount: 3,
-                name: "Twitter",
-                imp: "35",
-                clicks: "5",
-                revenue: "₦ 22,035.00",
-            },
-            {
-                bidAmount: 4,
-                name: "Google",
-                imp: "35",
-                clicks: "5",
-                revenue: "₦ 22,035.00",
-            },
-        ],
-    };
-
-    const dummyData2 = {
-        headers: [
-            { id: 1, text: "Customer" },
-            { id: 2, text: "CLTV" },
-            { id: 3, text: "Revenue" },
-        ],
-        rows: [
-            {
-                bidAmount: 1,
-                name: "Veres Panas",
-                cont: "202 Purchases",
-                ctv: "202",
-                revenue: "₦ 22,035.00",
-            },
-
-            {
-                bidAmount: 2,
-                name: "Mikso Fruzino",
-                cont: "202 Purchases",
-                ctv: "202",
-                revenue: "₦ 22,035.00",
-            },
-            {
-                bidAmount: 3,
-                name: "Mikso Fruzino",
-                cont: "202 Purchases",
-                ctv: "202",
-                revenue: "₦ 22,035.00",
-            },
-        ],
-    };
 
     const dummydum = [
         {
@@ -318,50 +244,6 @@ const DashboardPage = () => {
             id: "6",
             title: "Marketing Sales",
             topAmount: "12",
-            data: {
-                headers: [
-                    { id: 1, text: "Source" },
-                    { id: 2, text: "impressions" },
-                    { id: 3, text: "Clicks" },
-                    { id: 4, text: "Revenue" },
-                ],
-                rows: [
-                    {
-                        bidAmount: 1,
-                        name: "Facebook",
-                        imp: "35",
-                        clicks: "5",
-                        revenue: "₦ 22,035.00",
-                    },
-
-                    {
-                        bidAmount: 2,
-                        name: "Instagram",
-                        imp: "35",
-                        clicks: "5",
-                        revenue: "₦ 22,035.00",
-                    },
-                    {
-                        bidAmount: 3,
-                        name: "Twitter",
-                        imp: "35",
-                        clicks: "5",
-                        revenue: "₦ 22,035.00",
-                    },
-                    {
-                        bidAmount: 4,
-                        name: "Google",
-                        imp: "35",
-                        clicks: "5",
-                        revenue: "₦ 22,035.00",
-                    },
-                ],
-            },
-        },
-        {
-            id: "7",
-            title: "Returning Customer Rate",
-            topAmount: "18",
             data: {
                 headers: [
                     { id: 1, text: "Source" },
@@ -926,83 +808,6 @@ const DashboardPage = () => {
             {/* <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3"> */}
             <div className="container mx-auto mt-5">
                 <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-                    <div className="min-h-[240px] rounded-md border border-gray-300 bg-white p-6 shadow-sm">
-                        <div className="flex items-center justify-between">
-                            <h2 className="text-lg font-semibold">Store Sessions</h2>
-                            <img
-                                src={documentTextIcon}
-                                alt="Analytics Icon"
-                                className="w-4"
-                            />
-                        </div>
-
-                        <p className="mt-2 text-3xl font-bold">0 -</p>
-
-                        {/* card body */}
-
-                        <div>
-                            {" "}
-                            <div className="mt-3 flex items-center gap-2">
-                                <div>₦10</div>
-                                <div className="h-[2px] flex-1 bg-gray-300 line-through"></div>
-                            </div>
-                            <div className="mt-3 flex items-center gap-2">
-                                <div>₦10</div>
-                                <div className="h-[2px] flex-1 bg-gray-300 line-through"></div>
-                            </div>
-                            <div className="mt-3 flex items-center gap-2">
-                                <div>₦10</div>
-                                {/* <div className="flex flex-1 items-center justify-between">
-                                <div className="w-full border-t-2 border-dashed border-red-700"></div>
-                            </div> */}
-                                <div className="flex flex-1 items-center justify-between gap-2">
-                                    {" "}
-                                    {/* <div className="flex w-[30%] items-center">
-                                <div className="h-[2px] w-full bg-[#AE4551] line-through"></div>
-                            </div> */}
-                                    {/* <div className="flex justify-center font-bold text-[#E0B7BB]"> - - - - - - - - - - - -</div> */}
-                                    {/* <hr className="my-4 h-2 border-t-2 border-dashed border-gray-400" /> */}
-                                    <div className="flex w-full items-center space-x-2">
-                                        <div className="h-0.5 w-[35%] bg-[#AE4551]"></div>
-                                        <div className="h-0.5 w-[3%] bg-[#E0B7BB]"></div>
-                                        <div className="h-0.5 w-[3%] bg-[#E0B7BB]"></div>
-                                        <div className="h-0.5 w-[3%] bg-[#E0B7BB]"></div>
-                                        <div className="h-0.5 w-[3%] bg-[#E0B7BB]"></div>
-                                        <div className="h-0.5 w-[3%] bg-[#E0B7BB]"></div>
-                                        <div className="h-0.5 w-[3%] bg-[#E0B7BB]"></div>
-                                        <div className="h-0.5 w-[3%] bg-[#E0B7BB]"></div>
-                                        <div className="h-0.5 w-[3%] bg-[#E0B7BB]"></div>
-                                        <div className="h-0.5 w-[3%] bg-[#E0B7BB]"></div>
-                                        <div className="h-0.5 w-[3%] bg-[#E0B7BB]"></div>
-                                        <div className="h-0.5 w-[3%] bg-[#E0B7BB]"></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="mt-1 flex items-center gap-2">
-                                {/* <div>N10</div> */}
-                                <div className="t flex justify-between gap-4 pl-7 text-[12px] text-[#777F8C]">
-                                    <span className="text-[12px]">12:00AM</span>
-                                    <span className="text-[12px]">5:00AM </span>
-                                    <span className="text-[12px]">10:00AM </span>
-                                    <span className="text-[12px]">3:00PM</span>
-                                    <span className="text-[12px]">8:00PM</span>
-                                    {/* {"12:00AM  5:00AM  10:00AM  3:00PM 8:00PM "} */}
-                                </div>
-                            </div>
-                            <div className="mt-7 flex justify-end gap-4 text-sm text-gray-600">
-                                <div className="flex items-center gap-1">
-                                    <div className="h-[2px] w-4 bg-[#AE4551]"></div>
-                                    <span>Aug 19, 2024</span>
-                                </div>
-                                <div className="flex items-center gap-1">
-                                    <div className="w-4 border-t-2 border-dashed border-red-700 opacity-50"></div>
-                                    <span>Aug 18, 2024</span>
-                                </div>
-                            </div>
-                        </div>
-
-                        {/* card body stops here */}
-                    </div>
                     {/* Second Card */}
 
                     {dummydum.map((cell, idx) => (
@@ -1018,46 +823,6 @@ const DashboardPage = () => {
                     {/* third */}
 
                     {/* */}
-
-                    {/* <div className="mt-3 w-full overflow-x-auto pb-4">
-                        <table className="w-full overflow-x-auto">
-                            <thead className="bg-subtleGray w-full p-6">
-                                <tr className="bg-unusualGray w-full px-6">
-                                    <td class="p-4 text-xs font-semibold text-[#11192a]">Item </td>
-                                    <td class="p-4 text-xs font-semibold text-[#11192a]">Description </td>
-                                    <td class="p-4 text-xs font-semibold text-[#11192a]">Unit Price</td>
-                                    <td class="p-4 text-xs font-semibold text-[#11192a]">Quantity</td>
-                                    <td class="p-4 text-xs font-semibold text-[#11192a]">
-                                        Amount
-                                        <div> (before tax)</div>
-                                    </td>
-                                    <td class="p-4 text-xs font-semibold text-[#11192a]">Tax Rate</td>
-                                    <td class="p-4 text-xs font-semibold text-[#11192a]">Tax Amount</td>
-                                    <td class="p-4 text-xs font-semibold text-[#11192a]">Total Price</td>
-                                </tr>
-                            </thead>
-
-                            <tbody className=" ">
-                                {dummyData.map((cell, idx) => {
-                                    return (
-                                        <tr
-                                            key={idx}
-                                            className="w-full"
-                                        >
-                                            <td className="mt-2 p-4 text-xs font-semibold text-[#11192a]">{cell.item}</td>
-                                            <td className="mt-2 p-4 text-xs font-semibold text-[#11192a]">{cell.description}</td>
-                                            <td className="mt-2 p-4 text-xs font-semibold text-[#11192a]">{cell.unitPrice}</td>
-                                            <td className="mt-2 text-center text-xs font-semibold text-[#11192a]">{cell.Quantity}</td>
-                                            <td className="mt-2 p-4 text-xs font-semibold text-[#11192a]">{cell.amount}</td>
-                                            <td className="mt-2 p-4 text-xs font-semibold text-[#11192a]">{cell.taxRate}</td>
-                                            <td className="mt-2 p-4 text-xs font-semibold text-[#11192a]">{cell.taxAmount}</td>
-                                            <td className="mt-2 p-4 text-xs font-semibold text-[#11192a]">{cell.totalPrice}</td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </table>
-                    </div> */}
                 </div>
             </div>
 
